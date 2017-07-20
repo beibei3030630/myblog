00e717209b6eb45d2caea7e59d22fe38635c410c
beibei <1686310541@qq.com>
2017-07-20 17:19:24 +0800

    7/20commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-;$(function(){
-	
+$(function(){
 	var sidebar=$("#sidebar");
-	
-	var siderbar_trigger=$("#sidebar_trigger");
-	
-	siderbar_trigger.on("click",function(){
-		console.log("lala")
-	})
-})
+	var mask=$(".mask");
+	var sidebar_trigger=$("#siderbar_trigger");
+	var backButton=$(".back-to-top");
+  	function showSideBar(){
+	  	mask.fadeIn();
+		sidebar.animate({"right":0});
+  	}
+  	function hideSideBar(){
+  		mask.fadeOut();
+  		sidebar.css("right",-sidebar.width());
+  	}
+  sidebar_trigger.on("click",showSideBar);
+  sidebar.on('click', hideSideBar);
+  mask.on('click', hideSideBar);
+  backButton.on("click",function(){
+  		$("html,body").animate({
+  			scrollTop:0
+  		},800);
+  });
+  $(window).on("scroll",function(){
+  	if($(window).scrollTop()>$(window).height())
+  		backButton.fadeIn();
+  	else
+  		backButton.fadeOut();
+  });
+  $(window).trigger("scroll");
+});
